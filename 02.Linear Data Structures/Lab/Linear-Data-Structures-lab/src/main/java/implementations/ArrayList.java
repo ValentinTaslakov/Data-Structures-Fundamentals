9d45aa4890c9db23ddec8a9c6846024636aa8dce
ValentinTaslakov <79251472+ValentinTaslakov@users.noreply.github.com>
2022-05-05 16:08:14 +0300

    Implements Add method

--- 02.Linear Data Structures/Lab/Linear-Data-Structures-lab/src/main/java/implementations/ArrayList.java
+++ 02.Linear Data Structures/Lab/Linear-Data-Structures-lab/src/main/java/implementations/ArrayList.java
@@ -7,19 +7,44 @@ import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
 
-    @Override
-    public boolean add(E element) {
-        return false;
+    private static final int DEFAULT_CAPACITY = 4;
+    private Object[] elements;
+    private int elementsCount;
+
+    public ArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.elementsCount = 0;
     }
 
     @Override
+    public boolean add(E element) {
+
+        checkAndResizeIfNecessary();
+
+        this.elements[this.elementsCount] = element;
+        this.elementsCount++;
+        return true;
+    }
+
+
+    @Override
     public boolean add(int index, E element) {
-        return false;
+        indexValidation(index);
+        checkAndResizeIfNecessary();
+
+        E lastElement = this.get(elementsCount - 1);
+        for (int i = elementsCount - 1; i < index; i--) {
+
+        }
+
+        return true;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        indexValidation(index);
+
+        return (E) this.elements[index];
     }
 
     @Override
@@ -55,5 +80,23 @@ public class ArrayList<E> implements List<E> {
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+    private Object[] grow() {
+
+        return Arrays.copyOf(this.elements, this.elements.length * 2);
+    }
+
+    private void indexValidation(int index) {
+        if (index < 0 || index >= this.elements.length) {
+            throw new IndexOutOfBoundsException
+                    (String.format("Index %d is out of bounds for size %d%n", index, this.elements.length));
+        }
+    }
+
+    private void checkAndResizeIfNecessary() {
+        if (this.elementsCount == this.elements.length) {
+            this.elements = grow();
+        }
     }
 }
